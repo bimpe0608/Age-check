b2ff83f4d26117cb6709b6c7bd511d233efac1f2
bimpe <adeyinminuadebimpe@gmail.com>
2023-04-20 11:53:37 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Function to check if Individual is fit to drink alcohol
+function drinkingAge(name, age) {
+     age >= 16? console.log(`${name} is fit to drink alcohol`): console.log(`${name} is not fit to drink alcohol`);
+}
+
+drinkingAge("Bimpe", 23);
+
+// Function to check if Individual is fit to vote
+function legalVotingAge(name, age) {
+    age >= 18? console.log(`${name} is fit to vote`): console.log(`${name} is not fit to vote`)
+}
+
+legalVotingAge("Tobe", 22);
+
